5b84461ab355d8040c5099abbf0b5d7ba7e87867
GitKrih12 <bathu.prasad@ssspr.com>
2024-01-11 18:12:33 +0530

    added new code

--- sampleproj/test_sam.py
+++ sampleproj/test_sam.py
@@ -1,11 +1,11 @@
-class test_person:
+class def_test_Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
 
 
-p1 = test_person("laxman",1500)
-p2 = test_person("bharat",  1500)
+p1 = test_Person("laxman",1500)
+p2 = test_Person("bharat",  1500)
 
 print(p1.name)
 print(p1.age)
