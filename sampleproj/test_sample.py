5a9efeca940fbb185324a2726b114377c646cbf1
GitKrih12 <bathu.prasad@ssspr.com>
2024-02-11 17:30:07 +0530

    added new code

---
+++ sampleproj/test_sample.py
@@ -0,0 +1,20 @@
+
+import pytest
+from playwright.sync_api import sync_playwright
+
+@pytest.fixture
+
+
+def browser():
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        yield browser
+        browser.close()
+
+
+def test_open_browser(browser):
+    page = browser.new_page()
+    page.goto("https://www.github.com/")
+    page.screenshot(path="screenshots/example.png")
+    page.close()
